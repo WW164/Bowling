5a47c9a2d30bd63c50b92c43d48dcabf635efcd5
parinaz mirbagheri <parinazmirbagheri@gmail.com>
2020-11-30 01:50:21 -0800

    Add camera movement

---
+++ Assets/Scipts/Camera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    public float distance;
+    public float height;
+    public GameObject objectTofollow;
+
+    private void LateUpdate()
+    {
+
+        if (objectTofollow == null)
+            return;
+
+        Vector3 destionation = objectTofollow.transform.position;
+        destionation.x = 0f;
+        destionation.y += height;
+        destionation.z += distance;
+
+        transform.position = destionation;
+        
+    }
+}
